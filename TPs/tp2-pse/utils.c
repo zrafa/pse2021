a6c3af454dfd12806466e56dd97570624d7974ba
Rafael Zurita <rafaelignacio.zurita@gmail.com>
2021-05-05 08:24:35 -0300

    tp2

---
+++ TPs/tp2-pse/utils.c
@@ -0,0 +1,38 @@
+
+/* utils.c - funciones utiles al TP2 */
+
+/* Enciende y apaga un led conectado al puerto B bit 5 de un atmega328
+ * El puerto B de un atmega328 tiene los bits 0-5 mapeados a los 
+ * pines 8-13 de arduino 
+ */
+
+
+/* puertos de E/S */
+volatile unsigned char * puerto_b = (unsigned char *) 0x25; /* direccion de PORTB: SALIDA */
+volatile unsigned char * ddr_b = (unsigned char *) 0x24; /* direccion de DDR B (registro de control) */
+volatile unsigned char * pin_b = (unsigned char *) 0x23; /* direccion PIN B (registro de datos)*/
+
+
+/* 
+ * Configuración: Establecer el 5to bit del puerto B como entrada
+ *
+ * 	Poner en '0' el 5to bit de la dirección 0x24, que 
+ * 	es el la dirección del registro de control
+ *	(direccion de los datos) del puerto B (DDRB - Data Direction Register).
+ *      El 5to bit define la entrada o salida del
+ * 	pin del atmega328p que tiene conectado un led en una board arduino
+ */
+
+void esperar() {
+	volatile unsigned long i;
+	/* delay de 1 segundo */
+	for (i=0; i<450000; i++);
+}
+
+void prender_led() {
+
+}
+
+void apagar_led() {
+
+}
---
+++ TPs/tp2-pse/utils.h
@@ -0,0 +1,18 @@
+
+/* utils.h - funciones utiles al TP2 */
+
+/* 
+ * El puerto B de un atmega328 tiene los bits 0-5 mapeados a los 
+ * pines 8-13 de arduino 
+ */
+
+#ifndef _UTILS_H
+#define _UTILS_H
+
+
+void esperar();
+void prender_led();
+void apagar_led();
+
+
+#endif	/* _UTILS_H */
