d7c48269c2562779d3666103755b039f07db418f
Sergio Strazzacappa <sergio.strazzacappa@est.fi.uncoma.edu.ar>
2021-05-27 00:55:46 -0300

    Finalizado TP2

--- sergio.strazzacappa/tp2/src/knight_rider/main.c
+++ sergio.strazzacappa/tp2/src/knight_rider/main.c
@@ -5,40 +5,81 @@
 
 #include "utils.h"
 
+/*
+ * Cantidad de milisegundos entre el prendido y el apagado de
+ * un led.
+ */
+const int delay = 500;
+
+/*
+ * up = 1 si los leds se van encendiendo en orden ascendente, 
+ * up = 0 si los leds se van encendiendo en orden descendiente.
+ */
+char up = 1;
+
+/*
+ * on es el led que se va a encender.
+ * off es el led que se va a apagar, comienza en -1
+ * ya que al principio todos los leds están apagados y
+ * no es necesario apagar alguno.
+ */
+int on = 0;
+int off = -1;
+
+/*
+ * off_status = 1 cuando cambia el sentido de encendidos 
+ * (ascendente a descendente o viceversa) para que se apague el último led.
+ * En caso contrario es 0.
+ */
+
+char off_status = 0;
+
 int main(void)
 {
 	led_init();
-
-	int delay = 400;
-	int on = 0;
-	int off = -1;
 
 	while (1)
 	{
 		led_toggle(on, off); // Prende el led "on" y apaga el "off"
 		delay_ms(delay);	 // Espera "delay" milisegundos
 
-		if (on == 4)
+		// Llegó al primer led en orden descendente
+		if (on == 0 && !up)
 		{
-			on = 0; // Prende el último led, reinicia
+			up = 1; // Cambia el orden
+			off--;	// Apaga el primer led
+			off_status = 1;
 		}
-		else
+		else if (on == 4) // Llegó al último led en orden ascendente
 		{
-			on++; // Aumenta el led que se debe prender
+			up = 0; // Cambia el orden
+			off++;	// Apaga el último led
+			off_status = 1;
 		}
 
-		if (off == -1)
+		if (up) // Ascenso
 		{
-			off = 0; // Se debe apagar el primer led
+			on++; // Aumenta el led que se debe prender
+
+			if (off == -1)
+			{
+				off = 0; // Se debe apagar el primer led
+			}
+			else if (!off_status)
+			{
+				off++; // Aumenta el led que se debe apagar
+			}
 		}
-		else if (off == 4)
+		else // Descenso
 		{
-			off = 0; // Apaga el último led, reinicia
+			on--; // Decrementa el led que debe prender
+
+			if (!off_status)
+			{
+				off--;
+			}
 		}
-		else
-		{
-			off++; // Aumenta el led que se debe apagar
-		}
+		off_status = 0;
 	}
 
 	return 0;
--- sergio.strazzacappa/tp2/src/knight_rider/utils.c
+++ sergio.strazzacappa/tp2/src/knight_rider/utils.c
@@ -7,23 +7,26 @@
  * PB0 = 8
  */
 
+#define CYCLES_PER_MS (200);
+
+/*
+ * Arreglo con el bit que se debe apagar o prender para
+ * cada pin
+ */
 int led_bits[] = {
 	0x01, // Bit 0 - 0000 0001
 	0x02, // Bit 1 - 0000 0010
-	// 0x04, // Bit 2 - 0000 0100
 	0x08, // Bit 3 - 0000 1000
 	0x10, // bit 4 - 0001 0000
 	0x20  // bit 5 - 0010 0000
 };
 
-#define CYCLES_PER_MS (200);
+volatile unsigned char *DDR_B = (unsigned char *)0x24;
+volatile unsigned char *PORT_B = (unsigned char *)0x25;
 
 void led_init(void)
 {
-	volatile unsigned char *DDR_B = (unsigned char *)0x24;
-	volatile unsigned char *PORT_B = (unsigned char *)0x25;
-
-	for (int i = 0; i < 2; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		*DDR_B = *DDR_B | led_bits[i];	  // Setea el bit i de DDRB a 1 (output)
 		*PORT_B = *PORT_B & ~led_bits[i]; // Setea el bit i de PORTB a 0 (apaga)
@@ -34,7 +37,6 @@ void led_init(void)
 
 void led_toggle(int on, int off)
 {
-	volatile unsigned char *PORT_B = (unsigned char *)0x25;
 	unsigned char status;
 
 	status = *PORT_B;
